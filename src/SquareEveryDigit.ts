b74cc716933929aaa991721ff3a574d0bc117047
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2023-11-29 12:19:07 +0000

    refactor: simplify the square logic for a single number

--- src/SquareEveryDigit.ts
+++ src/SquareEveryDigit.ts
@@ -1,13 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export default class SquareEveryDigit {
   static squareDigits(input: number): number {
-    if (input === 2) {
-      return 4;
-    }
-    if (input === 3) {
-      return 9;
-    }
-
-    return 1;
+    return input ** 2;
   }
 }
